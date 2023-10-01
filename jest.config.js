80bcad9ef3699f497f39ead7ebdd599dd59bf31a
jonandereg <jonander.eg@gmail.com>
2023-10-01 20:38:15 +0200

    Initial setup based on next ts example

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  roots: ["<rootDir>/pages", "<rootDir>/components", "<rootDir>/utils"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+};
