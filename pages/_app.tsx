739930bc6c3c127e85c3b78791356c4274cb2d1a
jonandereg <jonander.eg@gmail.com>
2023-10-02 21:53:16 +0200

    removed extra layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,13 @@
 import { Provider } from "react-redux";
 import type { AppProps } from "next/app";
 import store from "../store";
-import Layout from "../components/Layout";
 import AuthProvider from "../components/AuthProvider";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <Provider store={store}>
       <AuthProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <Component {...pageProps} />
       </AuthProvider>
     </Provider>
   );
